7a8957c927af87301fed2d48f5008f5ba1f85e16
Cortney Lane <cdlane124@gmail.com>
2016-07-10 13:35:37 -0700

    fix npm lint command, got lastThingISaid() bonus to work!

--- lab-cortney/entry.js
+++ lab-cortney/entry.js
@@ -3,7 +3,6 @@
 require('!!file?name=[name].[ext]!./html/index.html');
 require('./scss/base.scss');
 
-// const $ = require('jquery');
 const angular = require('angular');
 const cowsay = require('cowsay-browser');
 
@@ -12,6 +11,7 @@ var app = angular.module('cowsayApp', []);
 app.controller('CowsayController', ['$scope', function($scope){
   $scope.cowsayText = 'put some text here!';
   $scope.cowsayFile = 'default';
+  $scope.historyOfThingsIveSaidArray = [];
 
   $scope.say = function(){
     let text = $scope.cowsayText || 'no, for real, put something here!';
@@ -19,8 +19,20 @@ app.controller('CowsayController', ['$scope', function($scope){
   };
 
   $scope.done = function(){
+    console.log('done');
     let text = $scope.cowsayText || 'no, for real, put something here!';
     $scope.result = cowsay.say({text: text, f: $scope.cowsayFile});
+    $scope.historyOfThingsIveSaidArray.push($scope.result);
+    console.log('history array', $scope.historyOfThingsIveSaidArray);
+  };
+
+  $scope.lastThingISaid = function(){
+    console.log('lastThingISaid');
+    var previousThing = $scope.historyOfThingsIveSaidArray.length - 1;
+    let text = $scope.historyOfThingsIveSaidArray[previousThing - 1];
+    // $scope.result = cowsay.say({text: text, f: $scope.cowsayFile});
+    $scope.result = text;
+    console.log('array post pop', $scope.historyOfThingsIveSaidArray);
   };
 
   cowsay.list(function(err, list){
