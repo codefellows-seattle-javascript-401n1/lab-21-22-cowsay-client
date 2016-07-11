6879f8a449b05d7d9cf2551297802c21e5d48a35
Doug Popadince <gsmatth@icloud.com>
2016-07-11 10:57:23 -0700

    debugging submit functionality

--- lab-doug/entry.js
+++ lab-doug/entry.js
@@ -4,9 +4,9 @@
 require('!!file?name=[name].[ext]!./app/html/index.html');
 require('./app/scss/base.scss');
 //npm modules
-// const angular = require('angular');
-// const cowsay = require('cowsay-browser');
+const angular = require('angular');
+const cowsay = require('cowsay-browser');
 // //module constants
-// const app = angular.module('cowsayApp', []);
+const app = angular.module('cowsayApp', []);
 // //app logic
-// const cowsayController = require('./app/cowsay.controller.js');
+const cowsayController = require('./app/cowsay.controller.js');
