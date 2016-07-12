d18bbff8955c6d948bc399d6fb583216066a71b8
FazioD <faziod@uw.edu>
2016-07-11 17:38:18 -0700

    added build and watch

--- lab-davide/entry.js
+++ lab-davide/entry.js
@@ -1,14 +1,24 @@
 'use strict';
 
-//webpack assets(html|images|css/scss|*)//
-
-
-//npm modules
-const $ = require('jquery');
-
-//app modules
-//app logic
-
-//basically is copying the below into the build directory//
+//webpack asset. is copying the below into the build directory//
 require('!!file?name=[name].[ext]!./html/index.html');
+
+
+require('./scss/base.scss'); //leave ext on sass file//
+
+//npm module//
+const angular = require('angular');
+// const cowsay = require('cowsay-browser');
+
+//defining a module. using an empty array because we are not using other angular modules that are dependent//
+var app = angular.module('cowsayApp', []);
+
+//fetching the same module from the angular cache//
+const app = angular.module('cowsayApp');
+
+
+// $().ready(function() {
+//   $('.cowsay').html('<pre>' + cowsay.say({text: 'This cow says more than MOO'}) + '</pre>');
+// });
+
 console.log('webpack is working');
--- lab-davide/webpack.config.js
+++ lab-davide/webpack.config.js
@@ -1,13 +1,19 @@
 'use strict';
 
-
-//the begining of the webpack config will remain the same. Variations will exist on config//
-//based on the tool trying to be built here//
+// the begining of the webpack config will remain the same. Variations will exist on config//
+// based on the tool trying to be built here//
 module.exports = {
   entry: `${__dirname}/entry.js`,
   output: {
     filename: 'bundle.js', //this is the filename that will be used after rendering//
     path: 'build' //if run webpack and this directory does not exist, will be created//
+  },
+  module: {
+    loaders: [   //these loaders run bttm to top, right to left
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass!'
+      }
+    ]
   }
-
 };
