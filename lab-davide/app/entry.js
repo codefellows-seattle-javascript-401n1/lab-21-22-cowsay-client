12e891a1b88e750f88dbe0e54a2238d48f6e90fc
FazioD <faziod@uw.edu>
2016-07-17 20:53:32 -0700

    adding functionality

---
+++ lab-davide/app/entry.js
@@ -0,0 +1,40 @@
+'use strict';
+
+//webpack asset. is copying the below into the build directory//
+require('!!file?name=[name].[ext]!./html/index.html');
+
+
+require('./scss/base.scss'); //leave ext on sass file//
+
+//npm module//
+const angular = require('angular');
+const cowsay = require('cowsay-browser');
+
+//defining a module. using an empty array because we are not using other angular modules that are dependent   on this module//
+var app = angular.module('cowsayApp', []);
+
+//$scope is a injectible predefined piece of functionality that we can add to a controller//
+app.controller('CowsayController', ['$scope', function($scope) {
+  $scope.cowsayText = 'Cows fall from the sky from trying to jump over the moon';
+  $scope.cowsayFile = 'default';
+  $scope.say = function() {
+    let text = $scope.cowsayText || 'Please fill this out, thx moo!';
+    return cowsay.say({text: text, f: $scope.cowsayFile});
+  };
+
+  $scope.done = function() {
+    let text = $scope.cowsayText || 'Please fill this out, thx moo!';
+    $scope.result = cowsay.say({text: text, f: $scope.cowsayFile});
+  };
+
+  cowsay.list(function(err, list) {
+    if(err) {
+      $scope.cowfiles = ['default'];
+      return;
+    }
+    $scope.cowfiles = list;
+  });
+}]);
+
+
+console.log('webpack is working');
--- lab-davide/webpack.config.js
+++ lab-davide/webpack.config.js
@@ -1,18 +1,31 @@
 'use strict';
 
+const extractPlugin = require('extract-text-webpack-plugin');
+
 // the begining of the webpack config will remain the same. Variations will exist on config//
 // based on the tool trying to be built here//
 module.exports = {
-  entry: `${__dirname}/entry.js`,
+  entry: `${__dirname}/app/entry.js`,
   output: {
     filename: 'bundle.js', //this is the filename that will be used after rendering//
     path: 'build' //if run webpack and this directory does not exist, will be created//
   },
+  plugins: [
+    new extractPlugin('bundle.css')
+  ],
   module: {
     loaders: [   //these loaders run bttm to top, right to left
       {
         test: /\.scss$/,
-        loader: 'style!css!sass!'
+        loader: extractPlugin.extract('style', 'css!sass!')
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015']
+        }
       }
     ]
   }
